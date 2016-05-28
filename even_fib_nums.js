530ad2d50a802f8adf13b5f3e52c6ea1491dd3c2
Bryce <bsaito88@gmail.com>
2016-05-28 11:10:27 -1000

    Passed all tests.

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,22 +7,51 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
-
+  var evenSum = 0;
+  var prevNum = 0;
+  var currNum = 1;
+  // var nextNum = 1;
+  while (sum <= maxFibValue) {
+    sum = prevNum + currNum;
+    prevNum = currNum;
+    currNum = sum;
+    if (sum % 2 === 0) {
+      evenSum += sum;
+      // console.log(evenSum);
+    }
+  }
   // do your work here
+    // for(var i = 1; sum <= maxFibValue; i++) {
+    //   fibArray.push(sum);
+    //   sum += fibArray[i - 1];
 
-  return sum;
+    //   console.log(sum, fibArray);
+    //   if (fibArray[i] % 2 === 0) {
+    //     evenSum += fibArray[i];
+    //     console.log(evenSum);
+    //   }
+    // }
+  return evenSum;
 }
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
-  var highest = 0;
-
+  // var highest = 0;
+  var sum = 0;
+  var prevNum = 0;
+  var currNum = 1;
+  // var nextNum = 1;
+  while (sum < maxFibValue) {
+    sum = prevNum + currNum;
+    prevNum = currNum;
+    currNum = sum;
+  }
   //define your base case, validate your input
 
 
   //do your work here
 
-  return highest;
+  return prevNum;
 };
 
 /**

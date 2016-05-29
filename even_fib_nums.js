21b5f5725aae0eac743405b4c84e5ae443834704
Lisa <misslisazhou@gmail.com>
2016-05-28 16:33:57 -1000

    create second way to pass first two tests of sum of even numbers

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -9,43 +9,30 @@ function _sumFibs( maxFibValue ) {
   var sum = 0;
   debugger;
   // do your work here
-  var previous = 0;
-  var current = 1;
-  var lastNum = 1;
-  while (lastNum <= maxFibValue){
-    if (current % 2 === 0){
-      sum +=current;
-    }
-    lastNum = previous + current;
-    previous = current;
-    current = lastNum;
-  }
-  return sum;
-
-  // //Version 1
-  // var fib = [0, 1];
-  // while (sum <= maxFibValue){
-  //   var newFib = fib[fib.length - 1] + fib[fib.length - 2];
-  //   fib.push(newFib);
-  //   //if the new sequence is even, add to the sum
-  //   if (newFib % 2 === 0){
-  //     sum += newFib;
+  // var previous = 0;
+  // var current = 1;
+  // var lastNum = 1;
+  // while (lastNum <= maxFibValue){
+  //   if (current % 2 === 0){
+  //     sum +=current;
   //   }
-  //   //if sum is equal to maxFibValue, the loop should end
-  //   if (sum === maxFibValue){
-  //     break;
-  //   }
-  //   //if the new sequence is equal to the maxFibValue, stop the loop; any additional even sequences will increase the sum, which should not exceed value of maxFibValue
-  //   if (newFib === maxFibValue){
-  //     break;
-  //   }
-  //   //if the new sequence is greater than the the maxFibValue and a new even sequence is added to sum that exceed value of maxFibValue, subtract that value from the sum, and stop the loop
-  //   if (newFib > maxFibValue && sum > maxFibValue){
-  //     sum -= newFib;
-  //     break;
-  //   }
+  //   lastNum = previous + current;
+  //   previous = current;
+  //   current = lastNum;
   // }
   // return sum;
+
+  //Version 2
+  var fib = [0, 1];
+  while (fib[fib.length - 1] <= maxFibValue){
+    var newFib = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.push(newFib);
+    //if the new sequence is even, add to the sum
+    if (newFib % 2 === 0){
+      sum += newFib;
+    }
+  }
+  return sum;
 }
 
 // bonus round

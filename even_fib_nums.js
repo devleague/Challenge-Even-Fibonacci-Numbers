e1c9a283fc9b8a7877a8231229459997b1fd0754
Lisa <misslisazhou@gmail.com>
2016-05-28 16:44:27 -1000

    pass 4th test

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,32 +7,32 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
-  debugger;
-  // do your work here
-  // var previous = 0;
-  // var current = 1;
-  // var lastNum = 1;
-  // while (lastNum <= maxFibValue){
-  //   if (current % 2 === 0){
-  //     sum +=current;
-  //   }
-  //   lastNum = previous + current;
-  //   previous = current;
-  //   current = lastNum;
-  // }
-  // return sum;
 
-  //Version 2
-  var fib = [0, 1];
-  while (fib[fib.length - 1] <= maxFibValue){
-    var newFib = fib[fib.length - 1] + fib[fib.length - 2];
-    fib.push(newFib);
-    //if the new sequence is even, add to the sum
-    if (newFib % 2 === 0){
-      sum += newFib;
+  //Version 1
+  var previous = 0;
+  var current = 1;
+  var lastNum = 1;
+  while (lastNum <= maxFibValue){
+    if (current % 2 === 0){
+      sum +=current;
     }
+    lastNum = previous + current;
+    previous = current;
+    current = lastNum;
   }
   return sum;
+
+  //Version 2
+  // var fib = [0, 1];
+  // while (fib[fib.length - 1] <= maxFibValue){
+  //   var newFib = fib[fib.length - 1] + fib[fib.length - 2];
+  //   fib.push(newFib);
+  //   //if the new sequence is even, add to the sum
+  //   if (newFib % 2 === 0){
+  //     sum += newFib;
+  //   }
+  // }
+  // return sum;
 }
 
 // bonus round
@@ -40,11 +40,17 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
-
+  var fib = [0, 1];
 
   //do your work here
-
-  return highest;
+  while (fib[fib.length - 1] <= maxFibValue){
+    highest = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.push(highest);
+    if (highest > maxFibValue){
+      highest = fib[fib.length - 2];
+      return highest;
+    }
+  }
 };
 
 /**

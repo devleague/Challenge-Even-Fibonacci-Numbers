3cd79985cc273f1efce2998e6812c4aa2c88bcb3
Christie R <creindle@hawaii.edu>
2016-05-28 18:43:11 -1000

    I need to fix up this code:)

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -8,9 +8,43 @@
 function _sumFibs( maxFibValue ) {
   var sum = 0;
 
-  // do your work here
-
-  return sum;
+  while (sum <= maxFibValue){
+    if(maxFibValue%2 === 0){
+      for (//start at end of list); while i > 0; i-3)
+        sum += //number at index;
+        return sum;//add number at index -3, iterate through array till index = 0
+      }
+    if (maxFibValue%2 !== 0){
+      //go back one index at a time, until you find number that is even
+      //then add that number to the sum
+    }
+    return sum;
+  }
+  // var sum = 0;
+  // if (maxFibValue === 0){
+  //   sum = 0;
+  //   return sum;
+  // }
+  // if (maxFibValue === 1){
+  //   sum = 0;
+  //   return sum;
+  // }
+  // if (maxFibValue === 2){
+  //   sum = 2;
+  //   return sum;
+  // }
+  // while (sum <= maxFibValue){
+  //   for (i=0;i ; i++){
+  //     if ((index-2)%2 === 0){
+  //       sum += index-2;
+  //     }
+  //     if ((index-1)%2 === 0){
+  //       sum += index-1;
+  //     }
+  //   }
+  //   return sum;
+  // }
+  // return sum;
 }
 
 // bonus round

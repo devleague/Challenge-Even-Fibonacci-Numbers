14690aeb45c0acfdacc3138fb7d8a3e698b85850
kekuso <kekuso808@gmail.com>
2016-05-28 14:47:53 -1000

    Completed even fibonacci numbers challenge

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -9,21 +9,51 @@ function _sumFibs( maxFibValue ) {
   var sum = 0;
 
   // do your work here
-
+  var fib = 0;
+  var fibNumbers = [];
+  fibNumbers[0] = 0;
+  fibNumbers[1] = 1;
+  for (var i = 1; fib <= maxFibValue; i++) {
+    fib = fibNumbers[i] + fibNumbers[i - 1];
+    if(fib <= maxFibValue) {
+      //console.log("Fib number:" + fib);
+      fibNumbers[i + 1] = fib;
+    }
+  }
+  // calculate the sum of all even Fibonacci numbers
+  for(i = 0; i < fibNumbers.length; i++) {
+    if(fibNumbers[i] % 2 === 0) {
+        //console.log("Adding " + fibNumbers[i]);
+        sum+=fibNumbers[i];
+        //console.log(sum);
+    }
+  }
+  //console.log(sum);
   return sum;
 }
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
-
   //define your base case, validate your input
 
-
   //do your work here
-
+  var fib = 0;
+  var fibNumbers = [];
+  fibNumbers[0] = 0;
+  fibNumbers[1] = 1;
+  var prevFib;
+  for (var i = 1; fib <= maxFibValue; i++) {
+    prevFib = fib;
+    fib = fibNumbers[i] + fibNumbers[i - 1];
+    if(fib <= maxFibValue) {
+      //console.log("Fib number:" + fib);
+      fibNumbers[i + 1] = fib;
+    }
+  }
+  highest = prevFib;
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
